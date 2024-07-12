386c59a4e8f71ac6a45439ab4174b07660119658
Manjeet Chhikara <mchhikara@Manjeets-MBP.attlocal.net>
2024-07-12 01:45:25 -0400

    renaming

--- src/main/java/com/manjeet/oauth2/OAuth2Application.java
+++ src/main/java/com/manjeet/oauth2/OAuth2Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LearnSsoApplication {
+public class OAuth2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LearnSsoApplication.class, args);
+		SpringApplication.run(OAuth2Application.class, args);
 	}
 
 }
